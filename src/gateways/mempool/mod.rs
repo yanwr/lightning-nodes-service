c08d1231511167f79ee1babb8948030f1ccca675
yanwr <yanywr@gmail.com>
2026-08-25 23:39:41 -0300

    chore: add gateway to state

--- src/gateways/mempool/mod.rs
+++ src/gateways/mempool/mod.rs
@@ -1,2 +1,16 @@
+use crate::{config::AppConfig, gateways::mempool::gateway::MempoolGateway};
+
 pub mod gateway;
 pub mod fetch_rankings_connectivity;
+
+pub struct Gateways {
+    pub mempool: MempoolGateway
+}
+
+impl Gateways {
+    pub fn new(app_config: &AppConfig) -> Result<Self, Box<dyn std::error::Error>> {
+        Ok(Self { 
+            mempool: MempoolGateway::new(app_config)?
+        })
+    }
+}
--- src/state.rs
+++ src/state.rs
@@ -1,14 +1,16 @@
 use sqlx::PgPool;
 
-use crate::config::AppConfig;
+use crate::{config::AppConfig, gateways::mempool::Gateways};
 
 pub struct AppState {
     pub postgres_pool: PgPool,
+    pub gateways: Gateways,
 }
 
 impl AppState {
     pub async fn create(app_config: AppConfig) -> Result<Self, Box<dyn std::error::Error>> {
         Ok(Self {
+            gateways: Gateways::new(&app_config)?,
             postgres_pool: app_config.database.create_pool().await?,
         })
     }
